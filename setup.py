5c0c17b55121e387399bcd5bfe853ccfeebf5c4f
ThaoNguyen15 <phuongthao.nguyen1993@gmail.com>
2016-08-12 16:38:58 -0400

    setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="crm",
+        version="0.1",
+        maintainer="TN",
+        maintainer_email="phuongthao.nguyen1993@gmail.com",
+        packages=find_packages()
+    )
+
