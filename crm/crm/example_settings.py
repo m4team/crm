68480eaa66b9974f4e940f4e01ca5fe758e6bf2d
ThaoNguyen15 <phuongthao.nguyen1993@gmail.com>
2016-08-14 21:27:14 -0400

    extra security setting

--- crm/crm/example_settings.py
+++ crm/crm/example_settings.py
@@ -124,3 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Security 
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
